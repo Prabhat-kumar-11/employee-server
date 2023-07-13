644cb11654ff47276f57ef9bf16b38a3b26be494
Prabhat Kumar <prabhssgg3@gmail.com>
2023-07-13 14:49:40 +0530

    backend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/userRoutes");
+
+const { auth } = require("./middleware/authMiddleware");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRouter);
+app.use(auth);
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("connected to the DB");
+  } catch (err) {
+    console.log(err);
+    console.log("cannot connect to the DB");
+  }
+  console.log(`Server is running at port ${process.env.port}`);
+});
---
+++ routes/userRoutes.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { UserModel } = require("../model/userModal");
+const userRouter = express.Router();
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+userRouter.post("signup", async (req, res) => {
+  const { email, password, confirmPassword } = req.body;
+
+  try {
+    bcrypt.hash(password, 5, async (err, hash) => {
+      const user = new UserModel({ email, password: hash, confirmPassword });
+      await user.save();
+      res.status(200).send("New user has been registered");
+    });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          const token = jwt.sign(
+            { authorID: user._id, author: user.name },
+            "employee"
+          );
+          console.log(token);
+          res.status(200).send({ msg: "login Successful", token: token });
+        } else {
+          res.status(200).send({ msg: "wrong Credentials" });
+        }
+      });
+    } else {
+      res.status(200).send({ msg: "wrong Credentials" });
+    }
+  } catch (err) {
+    res.status(400).send({ err: err.message });
+  }
+});
